b5bd393c8e6352f64ca6850dfbedbdc6746b08ab
m4ndr0 <119809627+m4ndr0@users.noreply.github.com>
2024-09-22 13:11:34 +0200

    Changing hero section and adding animations
    Changing hero section and adding animations.

---
+++ index.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show")
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll(".hidden")
+
+hiddenElements.forEach((el) => observer.observe(el))
+
+
+// SMOOTH SCROLLING USING ANCHOR TAGS //
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
